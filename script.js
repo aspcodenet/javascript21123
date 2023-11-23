30c06754ff60ba415bf510cffd6bb77aa0075730
Stefan Holmberg <stefan.holmberg@systementor.se>
2023-11-23 08:17:11 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//vad är en funktion
+//varför skriver vi funktioner
+//skillnad metod/funktion
+//hur deklarera man en funktion? Minst 4 olika sätt...of course!
+// function declaration
+// function expression
+// arrow function
+// vi stannar är just nu hehe...
+// BEST PRACTICE = function expression + arrow functions
+
+
+//Arrayer - alla jobbiga metoder!
+let ourCustomers = ["Hederlige Harrys Bilar AB", "Stark Industries", "Oscorp", "Pym Technologies", "The Daily Bugle"]
